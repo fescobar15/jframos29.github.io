7bddd950944d9e476ab1a464634ef98b612d4769
Juan Felipe Ramos Correa <jf.ramos@uniandes.edu.co>
2019-08-18 22:07:35 -0500

    Tested with eslinter

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-undef */
 "use strict";
 
+carga();
+cambiar();
+fondo();
 //Change abilities text in welcome div
 function carga(){
   let jobs = ["Developer", "Comm Engineer", "Music Lover", "Writer"];
   let i=0;
   setInterval(function(){
-      if(i==jobs.length){
-        i=0;
-      }
-      document.getElementById("abilities").innerText=jobs[i];
-      i++;
-    }, 3000);
+    if(i==jobs.length){
+      i=0;
+    }
+    document.getElementById("abilities").innerText=jobs[i];
+    i++;
+  }, 3000);
 }
 
 //Change family pictures
@@ -18,23 +22,23 @@ function cambiar(){
   let top=24;
   let i=1;
   setInterval(function(){
-      if(i==top){
-        i=1;
-      }
-      document.getElementById('family_photo').setAttribute('src','./images/'+i+'.jpg');
-      i++;
-    }, 1000);
+    if(i==top){
+      i=1;
+    }
+    document.getElementById("family_photo").setAttribute("src","./images/"+i+".jpg");
+    i++;
+  }, 1000);
 }
 
 //Change background color (JQuery)
 function fondo() {
   $(window).scroll(function () {
-     if ($(this).scrollTop() > 350) {
-        $('body').addClass('cambio')
-     }
-     if ($(this).scrollTop() < 350) {
-        $('body').removeClass('cambio')
-     }
+    if ($(this).scrollTop() > 350) {
+      $("body").addClass("cambio");
+    }
+    if ($(this).scrollTop() < 350) {
+      $("body").removeClass("cambio");
+    }
   });
 }
 
